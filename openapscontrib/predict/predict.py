f861dece382078ec0bf55155b777dfeb1f377179
Chris Hannemann <chris.hannemann@gmail.com>
2015-10-30 10:07:26 -0700

    Update predict.py

--- openapscontrib/predict/predict.py
+++ openapscontrib/predict/predict.py
@@ -305,7 +305,7 @@ def calculate_momentum_effect(
     glucose_slope, _, _, _, _ = linregress(fit_x, fit_y)
 
     for i, timestamp in enumerate(simulation_timestamps):
-        t = (timestamp - simulation_start).total_seconds()
+        t = max(0, (timestamp - last_glucose_datetime).total_seconds())
         momentum_effect[i] = t * glucose_slope
 
     return [{
